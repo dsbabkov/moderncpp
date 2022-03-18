42df9f8d364faa63b6adeaec965bd47766962551
dsbabkov <dsbabkov@users.noreply.github.com>
2022-03-19 00:20:48 +0300

    Ip filter (#1)
    * добавлена часть 2 и файл с ip
    
    * скопирован образец исходника
    
    * рефакторинг
    
    * рефакторинг
    
    * сортировка
    
    * Класс IpAddress
    
    * Код разнесён по файлам
    
    * Реализована фильтрация

---
+++ Task1/Part2/IpAddress.h
@@ -0,0 +1,22 @@
+#pragma once
+
+#include <string>
+
+class IpAddress {
+public:
+    explicit IpAddress(const std::string &str);
+
+    bool operator<(const IpAddress &o) const;
+
+    std::string toString() const;
+    uint32_t octet(size_t n) const;
+
+private:
+    static uint32_t octetStrToInt(std::string_view str);
+
+private:
+    uint32_t ip_ = 0;
+};
+
+std::ostream &operator<<(std::ostream &os, const IpAddress &ip);
+
---
+++ Task1/Part2/main.cpp
@@ -0,0 +1,108 @@
+#include <iostream>
+#include "IpAddress.h"
+#include "Split.h"
+
+int main(int argc, char const *argv[])
+{
+    try
+    {
+        std::vector<IpAddress> ip_pool;
+
+        for(std::string line; std::getline(std::cin, line);)
+        {
+            ip_pool.emplace_back(split(line, '\t').at(0));
+        }
+
+        std::sort(ip_pool.rbegin(), ip_pool.rend());
+
+        for(const auto &ip: ip_pool)
+        {
+            std::cout << ip << std::endl;
+        }
+
+        // 222.173.235.246
+        // 222.130.177.64
+        // 222.82.198.61
+        // ...
+        // 1.70.44.170
+        // 1.29.168.152
+        // 1.1.234.8
+
+        for(const auto &ip: ip_pool)
+        {
+            if (ip.octet(0) != 1) {
+                continue;
+            }
+            std::cout << ip << std::endl;
+        }
+
+        // 1.231.69.33
+        // 1.87.203.225
+        // 1.70.44.170
+        // 1.29.168.152
+        // 1.1.234.8
+
+        for(const auto &ip: ip_pool)
+        {
+            if (ip.octet(0) != 46 || ip.octet(1) != 70) {
+                continue;
+            }
+            std::cout << ip << std::endl;
+        }
+
+        // 46.70.225.39
+        // 46.70.147.26
+        // 46.70.113.73
+        // 46.70.29.76
+
+        for(const auto &ip: ip_pool)
+        {
+            if (ip.octet(0) != 46 && ip.octet(1) != 46 && ip.octet(2) != 46 && ip.octet(3) != 46) {
+                continue;
+            }
+            std::cout << ip << std::endl;
+        }
+        // ip = filter_any(46)
+
+        // 186.204.34.46
+        // 186.46.222.194
+        // 185.46.87.231
+        // 185.46.86.132
+        // 185.46.86.131
+        // 185.46.86.131
+        // 185.46.86.22
+        // 185.46.85.204
+        // 185.46.85.78
+        // 68.46.218.208
+        // 46.251.197.23
+        // 46.223.254.56
+        // 46.223.254.56
+        // 46.182.19.219
+        // 46.161.63.66
+        // 46.161.61.51
+        // 46.161.60.92
+        // 46.161.60.35
+        // 46.161.58.202
+        // 46.161.56.241
+        // 46.161.56.203
+        // 46.161.56.174
+        // 46.161.56.106
+        // 46.161.56.106
+        // 46.101.163.119
+        // 46.101.127.145
+        // 46.70.225.39
+        // 46.70.147.26
+        // 46.70.113.73
+        // 46.70.29.76
+        // 46.55.46.98
+        // 46.49.43.85
+        // 39.46.86.85
+        // 5.189.203.46
+    }
+    catch(const std::exception &e)
+    {
+        std::cerr << e.what() << std::endl;
+    }
+
+    return 0;
+}
