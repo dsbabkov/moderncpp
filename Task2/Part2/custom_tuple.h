43124a33f5b3d033a54d73c37bd451aeb166f773
Dmitriy Babkov <dsbabkov@gmail.com>
2022-03-21 20:52:50 +0300

    rename

--- Task2/Part2/custom_tuple.h
+++ Task2/Part2/custom_tuple.h
@@ -4,55 +4,55 @@
 #include "strip_ref.h"
 
 template <typename T, typename ...Rest>
-class custom_tupple: public custom_tupple<Rest...> {
+class custom_tuple: public custom_tuple<Rest...> {
 public:
-    explicit custom_tupple(const T &t, const Rest &... rest);
+    explicit custom_tuple(const T &t, const Rest &... rest);
 
     template<typename U, typename ...Rest2>
-    custom_tupple &operator=(const custom_tupple<U, Rest2...> &o);
+    custom_tuple &operator=(const custom_tuple<U, Rest2...> &o);
 
     strip_ref_t<T> t_;
 };
 
 template <typename T>
-class custom_tupple<T> {
+class custom_tuple<T> {
 public:
-    explicit custom_tupple(const T &t);
+    explicit custom_tuple(const T &t);
 
     template<typename U>
-    custom_tupple &operator=(const custom_tupple<U> &o);
+    custom_tuple &operator=(const custom_tuple<U> &o);
 
     strip_ref_t<T> t_;
 };
 
 template<typename T>
-custom_tupple<T>::custom_tupple(const T &t)
+custom_tuple<T>::custom_tuple(const T &t)
         : t_(t) {
 }
 
 template<typename T>
 template<typename U>
-custom_tupple<T> &custom_tupple<T>::operator=(const custom_tupple<U> &o) {
+custom_tuple<T> &custom_tuple<T>::operator=(const custom_tuple<U> &o) {
     t_ = o.t_;
     return *this;
 }
 
 template<typename T, typename... Rest>
-custom_tupple<T, Rest...>::custom_tupple(const T &t, const Rest &... rest)
-        : custom_tupple<Rest...>(rest...),
+custom_tuple<T, Rest...>::custom_tuple(const T &t, const Rest &... rest)
+        : custom_tuple<Rest...>(rest...),
           t_(t) {
 
 }
 
 template<typename T, typename... Rest>
 template<typename U, typename... Rest2>
-custom_tupple<T, Rest...> &custom_tupple<T, Rest...>::operator=(const custom_tupple<U, Rest2...> &o) {
+custom_tuple<T, Rest...> &custom_tuple<T, Rest...>::operator=(const custom_tuple<U, Rest2...> &o) {
     t_ = o.t_;
-    *static_cast<custom_tupple<Rest...>*>(this) = *static_cast<const custom_tupple<Rest2...>*>(&o);
+    *static_cast<custom_tuple<Rest...>*>(this) = *static_cast<const custom_tuple<Rest2...>*>(&o);
     return *this;
 }
 
 template <typename ...Args>
-custom_tupple<strip_ref_t<Args>...> make_custom_tuple(const Args &...args) {
-    return custom_tupple<strip_ref_t<Args>...>(args...);
+custom_tuple<strip_ref_t<Args>...> make_custom_tuple(const Args &...args) {
+    return custom_tuple<strip_ref_t<Args>...>(args...);
 }
