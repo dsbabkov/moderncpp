038599adc8a564bed55bd36bd753adc84eaa16e8
Dmitriy Babkov <dsbabkov@gmail.com>
2022-03-31 23:55:24 +0300

    using init list

--- Task3/main.cpp
+++ Task3/main.cpp
@@ -1,17 +1,32 @@
 #include <map>
 #include <iostream>
+#include <memory>
 
-template <typename T>
+
+template <typename T, size_t MaxObjectsAtATime>
 struct MyAllocator {
     using value_type = T;
+    MyAllocator() = default;
+
+    template<typename U>
+    MyAllocator(const MyAllocator<U, MaxObjectsAtATime> &) {
+
+    }
 
     template <typename U>
     struct rebind {
-        using other = MyAllocator<U>;
+        using other = MyAllocator<U, MaxObjectsAtATime>;
     };
 
-    T *allocate(std::size_t n) noexcept {
+    T *allocate(std::size_t n)  {
+        if (n > MaxObjectsAtATime) {
+            std::cerr << n << " object(s) requested to allocate. Max: " << MaxObjectsAtATime << '\n';
+            throw std::bad_alloc();
+        }
         T *result = static_cast<T *>(std::malloc(sizeof(T) * n));
+        if (!result) {
+            throw std::bad_alloc();
+        }
         std::cout << "Allocate " << n << " element(s) of type " << typeid(T).name() << " at " << result << '\n';
         return result;
     }
@@ -23,6 +38,9 @@ struct MyAllocator {
 };
 
 int main() {
-    std::map<int, int, std::less<int>, MyAllocator<std::pair<const int, int>>> m;
-    m.emplace(1, 1);
+    std::map<int, int, std::less<int>, MyAllocator<std::pair<const int, int>, 1>> m = {
+            {1, 5},
+            {2, 3},
+            {3, 5},
+    };
 }
